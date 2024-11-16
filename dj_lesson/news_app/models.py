5a8a713456cb0258d86481b623231f66259118dd
janisfs <janisfs@mail.ru>
2024-11-16 22:43:56 +0300

    add new code, models db file

--- dj_lesson/news_app/models.py
+++ dj_lesson/news_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название', max_length=100)
+    short_description = models.CharField('Краткое описание', max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
